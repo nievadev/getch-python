add63b25fe662ceb5b5913635a26bf88a74ced2e
Martín Nieva <martinjafactor@gmail.com>
2019-11-02 23:57:23 -0300

    Added chars param and changed some variable

--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 import termios, sys, tty
 
-def getch():
+def getch(chars):
     file_descriptor = sys.stdin.fileno()
 
     terminal_settings = termios.tcgetattr(file_descriptor)
 
     tty.setraw(file_descriptor) # Setting raw mode
 
-    character = sys.stdin.read(6)
+    character = sys.stdin.read(chars)
 
-    termios.tcsetattr(file_descriptor, termios.TCSADRAIN, actual_settings) # Reset terminal to normal state
+    termios.tcsetattr(file_descriptor, termios.TCSADRAIN, terminal_settings) # Reset terminal to normal state
 
     return character
 
-ch = getch()
+ch = getch(10)
 print(ch)
